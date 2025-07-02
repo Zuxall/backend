725725bb306609ff596193f6f89abdd04e7745bb
Zuxall <marvinventura.33240@gmail.com>
2025-07-02 04:52:47 +0200

    Update db.js

--- src/config/db.js
+++ src/config/db.js
@@ -1,10 +1,10 @@
 // src/config/db.js
-require('dotenv').config();
+require('dotenv').config();   // ne chargera rien en prod car .env n'existe plus
 const { Sequelize } = require('sequelize');
 
 const url = process.env.DATABASE_URL;
 if (!url) {
-  console.error("❌ DATABASE_URL n'est pas défini !");
+  console.error("❌ DATABASE_URL manquant !");
   process.exit(1);
 }
 
@@ -13,8 +13,7 @@ const sequelize = new Sequelize(url, {
   logging: false
 });
 
-sequelize
-  .authenticate()
+sequelize.authenticate()
   .then(() => console.log("✅ DB connected via DATABASE_URL"))
   .catch(err => {
     console.error("❌ DB connection failed:", err);
