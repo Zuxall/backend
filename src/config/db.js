a68dad767a4783e17e7d32d0a63c8ec60acf3c98
Zuxall <marvinventura.33240@gmail.com>
2025-07-02 03:39:52 +0200

    Update db.js

--- src/config/db.js
+++ src/config/db.js
@@ -1,12 +1,16 @@
+// src/config/db.js
+
 require('dotenv').config();
+
 const { Sequelize } = require('sequelize');
 
 const sequelize = new Sequelize(
-  process.env.DB_NAME,
-  process.env.DB_USER,
-  process.env.DB_PASS,
+  process.env.DB_NAME,       // ex. 'railway'
+  process.env.DB_USER,       // ex. 'root'
+  process.env.DB_PASS,       // ex. 'vNmaSCbWNRElNwbw…'
   {
-    host: process.env.DB_HOST,
+    host: process.env.DB_HOST,       // → doit être mysql.railway.internal
+    port: process.env.DB_PORT,       // → 3306
     dialect: 'mysql',
     logging: false
   }
